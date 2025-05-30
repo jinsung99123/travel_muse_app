b47267e5a8e6b1107e9ad43214ae77cecb402822
MJ <gwang907@gmail.com>
2025-05-30 19:27:03 +0900

    refactor: snsLoginBar 위젯 분리

--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -1,67 +1,38 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:travel_muse_app/views/login/widgets/sns_login_bar.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          SizedBox(height: screenHeight * 0.1),
           Text(
-            'Tripmuse',
+            'TravelMuse',
             style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          SizedBox(height: screenHeight * 0.1),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
             width: double.infinity,
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
             child: Column(
               children: [
-                GestureDetector(
-                  onTap: () {
-                    log('tap: Google Login');
-                    // TODO: Google Login
-                  },
-                  child: snsLoginBar(
-                    sns: 'Google',
-                    backgroundColor: Colors.blue,
-                    textColor: Colors.white,
-                  ),
+                SnsLoginBar(
+                  sns: 'Google',
+                  backgroundColor: Colors.blue,
+                  textColor: Colors.white,
+                  loginFunction: () async {}, // SignWithGoogle
                 ),
               ],
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Container snsLoginBar({
-    required String sns,
-    required Color backgroundColor,
-    required Color textColor,
-  }) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Center(
-        child: Text(
-          'Sign in with $sns',
-          style: TextStyle(
-            fontSize: 18,
-            color: textColor,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
       ),
     );
   }
