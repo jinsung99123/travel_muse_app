a3428c2186f47b25a59b20906122c529953f55e5
jinsung99123 <felixson99@gmail.com>
2025-06-02 11:34:53 +0900

    feat: 스플레시 페이지 ui구현

---
+++ lib/views/splash/splash_page.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:travel_muse_app/views/my_page/my_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            Text('로딩 중', style: TextStyle(fontSize: 18)),
+          ],
+        ),
+      ),
+    );
+  }
+}
