2604aa38add1171d9815182425f5b3a43cda08ad
jinsung99123 <felixson99@gmail.com>
2025-06-02 18:31:59 +0900

    feat: table_calendar 패키지 설치 및 캘린더 기본 구조 구현

---
+++ lib/views/calendar/calendar_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key});
+
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime _focusedDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('달력'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TableCalendar(
+          firstDay: DateTime.utc(2000, 1, 1),
+          lastDay: DateTime.utc(2100, 12, 31),
+          focusedDay: _focusedDay,
+          onDaySelected: (_, __) {}, // 클릭 이벤트는 아직 없음
+        ),
+      ),
+    );
+  }
+}
