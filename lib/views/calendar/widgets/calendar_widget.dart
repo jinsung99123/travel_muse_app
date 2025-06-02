12e90e42643fe18eb7e2967b466e2ad9348e2db5
jinsung99123 <felixson99@gmail.com>
2025-06-02 19:14:12 +0900

    feat: 캘린더 위젯 주석 추가

--- lib/views/calendar/widgets/calendar_widget.dart
+++ lib/views/calendar/widgets/calendar_widget.dart
@@ -12,21 +12,31 @@ class CalendarWidget extends StatelessWidget {
     required this.onDaySelected,
   });
   final CalendarState state;
+
+  // 특정 날짜가 선택된 날짜인지 확인
   final bool Function(DateTime) isSelected;
+
+  // 특정 날짜가 선택된 날짜 사이에 있는지 확인
   final bool Function(DateTime) isBetween;
+
   final void Function(DateTime, DateTime) onDaySelected;
 
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
+      // 선택 가능한 날짜 범위
       firstDay: DateTime.utc(2000, 1, 1),
       lastDay: DateTime.utc(2100, 12, 31),
+
       focusedDay: state.focusedDay,
       selectedDayPredicate: isSelected,
       onDaySelected: onDaySelected,
+
+      // 날짜 셀을 커스터마이징 하기 위한 빌더
       calendarBuilders: CalendarBuilders(
         defaultBuilder:
             (context, day, _) => buildDayCell(day, isBetween(day), false),
+
         selectedBuilder: (context, day, _) => buildDayCell(day, false, true),
       ),
     );
