3c375fe807ff82da8f7b7a1e7ac534706d9436d9
ghghtls <ghghtls2@gmail.com>
2025-06-02 21:10:58 +0900

    feat:고라우터 작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:travel_muse_app/views/home/home_page.dart';
+import 'package:travel_muse_app/views/location/map_page.dart';
 import 'package:travel_muse_app/views/login/login_page.dart';
+import 'package:travel_muse_app/views/my_page/my_page.dart';
+import 'package:travel_muse_app/views/onboarding/onboarding_page.dart';
+import 'package:travel_muse_app/views/plan/place_search/place_search_page.dart';
+import 'package:travel_muse_app/views/plan/schedule/schedule_page.dart';
+import 'package:travel_muse_app/views/preference/preference_test_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -9,9 +17,43 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage());
+    final router = GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(path: '/', builder: (context, state) => const LoginPage()),
+        GoRoute(path: '/home', builder: (context, state) => const HomePage()),
+        GoRoute(
+          path: '/schedule',
+          builder: (context, state) => const SchedulePage(),
+        ),
+        GoRoute(
+          path: '/place_search',
+          builder: (context, state) => const PlaceSearchPage(),
+        ),
+        GoRoute(
+          path: '/location',
+          builder: (context, state) => const MapPage(),
+        ),
+        GoRoute(path: '/mypage', builder: (context, state) => const MyPage()),
+
+        GoRoute(
+          path: '/onboarding',
+          builder: (context, state) => const OnboardingPage(),
+        ),
+        GoRoute(
+          path: '/preference_test',
+          builder: (context, state) => const PreferenceTestPage(),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      title: 'Travel Muse App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
   }
 }
