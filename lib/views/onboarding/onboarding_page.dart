fd8b17e4c6255e08d4a9e8914bcbab9a15ab4b0a
MJ <gwang907@gmail.com>
2025-06-02 12:03:41 +0900

    feat: nextButton 위젯 수정

--- lib/views/onboarding/onboarding_page.dart
+++ lib/views/onboarding/onboarding_page.dart
@@ -21,52 +21,51 @@ class OnboardingPage extends ConsumerWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView(
-              controller: viewModel.pageController,
-              physics: const NeverScrollableScrollPhysics(),
-              onPageChanged: viewModel.setPageIndex,
-              children: viewModel.pages,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                controller: viewModel.pageController,
+                physics: const NeverScrollableScrollPhysics(),
+                onPageChanged: viewModel.setPageIndex,
+                children: viewModel.pages,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: nextButton(state, viewModel),
-            ),
-          ),
-          const SizedBox(height: 30),
-        ],
+            nextButton(state, viewModel),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
   // ui 종료
 
   // 위젯
-  ElevatedButton nextButton(
-    OnboardingState state,
-    OnboardingViewModel viewModel,
-  ) {
-    return ElevatedButton(
-      onPressed:
+  Widget nextButton(OnboardingState state, OnboardingViewModel viewModel) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed:
+            state.currentPageIndex != viewModel.pages.length - 1
+                ? viewModel.nextPage
+                : () {
+                  // TODO: 성향 테스트 시작
+                },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xff025ADF),
+        ),
+        child: Text(
           state.currentPageIndex != viewModel.pages.length - 1
-              ? viewModel.nextPage
-              : () {
-                // TODO: 성향 테스트 시작
-              },
-      style: ElevatedButton.styleFrom(backgroundColor: const Color(0xff025ADF)),
-      child: Text(
-        state.currentPageIndex != viewModel.pages.length - 1
-            ? '다음'
-            : '성향 테스트 시작하기',
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+              ? '다음'
+              : '성향 테스트 시작하기',
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
     );
--- lib/views/onboarding/tabs/onboarding_tab_1.dart
+++ lib/views/onboarding/tabs/onboarding_tab_1.dart
@@ -9,14 +9,11 @@ class OnboardingTab1 extends StatelessWidget {
       children: [
         Text('닉네임을 입력해 주세요.', style: TextStyle(fontSize: 20)),
         SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: TextFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
+        TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey, width: 1),
+              borderRadius: BorderRadius.circular(10),
             ),
           ),
         ),
