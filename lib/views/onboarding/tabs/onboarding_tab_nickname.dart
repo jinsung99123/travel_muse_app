8a7e321fabe2be3c6156277a446d9f82cb77ab57
jinsung99123 <felixson99@gmail.com>
2025-06-04 11:55:49 +0900

    fix: 리베이스

--- lib/views/onboarding/tabs/onboarding_tab_nickname.dart
+++ lib/views/onboarding/tabs/onboarding_tab_nickname.dart
@@ -8,14 +8,9 @@ class OnboardingTabNickname extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-<<<<<<< HEAD
         const Text('닉네임을 입력해 주세요.', style: TextStyle(fontSize: 20)),
         const SizedBox(height: 20),
         // TODO: 텍스트 컨트롤러 관리 - 생성, 할당, dispose
-=======
-        Text('닉네임을 입력해 주세요.', style: TextStyle(fontSize: 20)),
-        SizedBox(height: 20),
->>>>>>> 53b23cc (feat: 프로필 수정 위젯 온보딩 탭에 적용)
         EditNickname(),
       ],
     );
