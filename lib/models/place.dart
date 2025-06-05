f3b2619180f2d4a035f04be980ff58d022d675e4
jinju9513 <wjswlswn99@naver.com>
2025-06-05 12:23:58 +0900

    feat: Place 모델 클래스 정의 및 fromKakaoJson 매핑 추가

---
+++ lib/models/place.dart
@@ -0,0 +1,37 @@
+class Place {
+  final String id;
+  final String name;
+  final String address;
+  final String city;
+  final String district;
+  final double latitude;
+  final double longitude;
+  final String category;
+
+  Place({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.city,
+    required this.district,
+    required this.latitude,
+    required this.longitude,
+    required this.category,
+  });
+
+  factory Place.fromKakaoJson(Map<String, dynamic> json) {
+    final address = json['address_name'] ?? '';
+    final addressParts = address.split(' ');
+
+    return Place(
+      id: json['id'] ?? '',
+      name: json['place_name'] ?? '',
+      address: address,
+      city: addressParts.length > 0 ? addressParts[0] : '',
+      district: addressParts.length > 1 ? addressParts[1] : '',
+      latitude: double.tryParse(json['y'] ?? '0') ?? 0,
+      longitude: double.tryParse(json['x'] ?? '0') ?? 0,
+      category: json['category_name'] ?? '',
+    );
+  }
+}
