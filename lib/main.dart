74530ca478c9158e71e6e4d8484898ca5960847c
jinsung99123 <felixson99@gmail.com>
2025-06-05 11:04:37 +0900

    fix: 라우터 파일 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:travel_muse_app/firebase_options.dart';
-import 'package:travel_muse_app/views/home/home_page.dart';
-import 'package:travel_muse_app/views/location/map_page.dart';
-import 'package:travel_muse_app/views/login/login_page.dart';
-import 'package:travel_muse_app/views/my_page/my_page.dart';
-import 'package:travel_muse_app/views/onboarding/onboarding_page.dart';
-import 'package:travel_muse_app/views/plan/place_search/place_search_page.dart';
-import 'package:travel_muse_app/views/plan/schedule/schedule_page.dart';
-import 'package:travel_muse_app/views/preference/preference_test_page.dart';
+import 'package:travel_muse_app/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,36 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final router = GoRouter(
-      initialLocation: '/',
-      routes: [
-        GoRoute(path: '/', builder: (context, state) => const LoginPage()),
-        GoRoute(path: '/home', builder: (context, state) => const HomePage()),
-        GoRoute(
-          path: '/schedule',
-          builder: (context, state) => const SchedulePage(),
-        ),
-        GoRoute(
-          path: '/place_search',
-          builder: (context, state) => const PlaceSearchPage(),
-        ),
-        GoRoute(
-          path: '/location',
-          builder: (context, state) => const MapPage(),
-        ),
-        GoRoute(path: '/mypage', builder: (context, state) => const MyPage()),
-
-        GoRoute(
-          path: '/onboarding',
-          builder: (context, state) => const OnboardingPage(),
-        ),
-        GoRoute(
-          path: '/preference_test',
-          builder: (context, state) => const PreferenceTestPage(),
-        ),
-      ],
-    );
-
     return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
