7174f4c1277b1f1343f58d8f81a0f17d27a4ae40
jinju9513 <wjswlswn99@naver.com>
2025-06-09 18:39:59 +0900

    feat: Firestore에서 플랜 및 경로 데이터를 불러오는 mapRepository 구현

--- lib/providers/map_provider.dart
+++ lib/providers/map_provider.dart
@@ -3,12 +3,12 @@ import 'package:travel_muse_app/models/map_state.dart';
 import 'package:travel_muse_app/repositories/map_repository.dart';
 import 'package:travel_muse_app/viewmodels/map_view_model.dart';
 
-final planRepositoryProvider = Provider<MapRepository>((ref) {
+final mapRepositoryProvider = Provider<MapRepository>((ref) {
   return MapRepository();
 });
 
 final mapViewModelProvider =
     StateNotifierProvider<MapViewModel, MapState>((ref) {
-  final repo = ref.watch(planRepositoryProvider);
+  final repo = ref.watch(mapRepositoryProvider);
   return MapViewModel(repo);
 });
