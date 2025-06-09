4185c26c26212110068b09d564d3c818a439c27c
MJ <gwang907@gmail.com>
2025-06-09 22:03:27 +0900

    feat: 닉네임 텍스트폼필드 조정(WIP)

--- lib/core/validators.dart
+++ lib/core/validators.dart
@@ -2,14 +2,14 @@ class Validators {
   // 닉네임
   static String? validateNickname(String? value) {
     if (value == null || value.trim().isEmpty) {
-      return '닉네임은 2글자 이상이어야 하며 한글, 숫자, 영문만 입력 가능합니다.';
+      return '한글, 영문, 숫자만 사용 가능 (2~8자)';
     }
 
     final nickname = value.trim();
 
     final validPattern = RegExp(r'^[가-힣a-zA-Z0-9]+$');
     if (!validPattern.hasMatch(nickname) || nickname.length < 2) {
-      return '닉네임은 2글자 이상이어야 하며 한글, 숫자, 영문만 입력 가능합니다.';
+      return '한글, 영문, 숫자만 사용 가능 (2~8자)';
     }
 
     return null;
--- lib/views/widgets/edit_nickname.dart
+++ lib/views/widgets/edit_nickname.dart
@@ -26,21 +26,37 @@ class EditNickname extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 8),
             child: Form(
               key: formKey,
-              child: TextFormField(
-                controller: controller,
-                validator: Validators.validateNickname,
-                decoration: InputDecoration(
-                  hintText: '닉네임', // TODO: 유저 기존 닉네임 연결, 없을 시 표시 X
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  helper: Text(
-                    '최대 8자까지 작성 가능합니다', // TODO : 유저 닉네임 validator 통과 시 표시 X
-                    style: TextStyle(
+              child: SizedBox(
+                height: 80,
+                child: TextFormField(
+                  controller: controller,
+                  validator: Validators.validateNickname,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 28,
+                      horizontal: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    isDense: true,
+                    hintText: '닉네임', // TODO: 유저 기존 닉네임 연결, 없을 시 표시 X
+                    // helper: Text(
+                    //   '최대 8자까지 작성 가능합니다', // TODO : 유저 닉네임 validator 통과 시 표시 X
+                    //   style: TextStyle(
+
+                    //     fontSize: 14,
+                    //     fontWeight: FontWeight.w400,
+                    //     color: Color(0xFF7C878C),
+                    //   ),
+                    // ),
+                    // validator 반환 텍스트 스타일
+                    errorStyle: TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.w400,
-                      color: Color(0xFF7C878C),
+                      color: Colors.redAccent,
                     ),
                   ),
                 ),
