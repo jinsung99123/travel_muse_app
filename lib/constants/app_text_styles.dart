119920ee77746e19b0c8e9634cb9496bd80d80da
MJ <gwang907@gmail.com>
2025-06-10 16:15:42 +0900

    chore: 앱 상수 컬러, 텍스트스타일 추가

---
+++ lib/constants/app_text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  // 온보딩 각 세션 타이틀
+  static const TextStyle sectionTitle = TextStyle(
+    color: Color(0xFF26272A),
+    fontSize: 16,
+    fontFamily: 'Pretendard',
+  );
+
+  // 텍스트폼필드 헬퍼텍스트
+  static const TextStyle helperText = TextStyle(
+    color: Color(0xFF7C878C),
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+  );
+
+  // 텍스트폼필드 에러텍스트
+  static const TextStyle errorText = TextStyle(
+    color: Color(0xFFFF6054),
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+  );
+}
