dc4f8e4a6b19e06d79dee96980cf9282efe45454
MJ <gwang907@gmail.com>
2025-06-16 23:12:48 +0900

    rebase: rebase develop

--- lib/views/my_page/preference_list_page.dart
+++ lib/views/my_page/preference_list_page.dart
@@ -6,12 +6,7 @@ import 'package:travel_muse_app/models/preference_test_model.dart';
 import 'package:travel_muse_app/providers/preference_test_provider.dart';
 import 'package:travel_muse_app/viewmodels/profile_view_model.dart';
 import 'package:travel_muse_app/views/my_page/widgets/my_page_list_item.dart';
-import 'package:travel_muse_app/views/preference/preference_loading_page.dart';
-<<<<<<< HEAD
-import 'package:travel_muse_app/views/preference/preference_test_page.dart';
 import 'package:travel_muse_app/views/preference/widgets/result_view.dart';
-=======
->>>>>>> 800b61a (feat: 성향 테스트 결과 리스트 db연결)
 
 class PreferenceListPage extends ConsumerStatefulWidget {
   const PreferenceListPage({super.key});
@@ -51,9 +46,7 @@ class _PreferenceListPageState extends ConsumerState<PreferenceListPage> {
           padding: const EdgeInsets.all(10),
           child:
               _tests.isEmpty
-                  ? Center(
-                    child: Text('여행 성향 테스트 결과가 없습니다.'),
-                  ) 
+                  ? Center(child: Text('여행 성향 테스트 결과가 없습니다.'))
                   : ListView.builder(
                     itemCount: _tests.length,
                     itemBuilder: (context, index) {
@@ -61,7 +54,6 @@ class _PreferenceListPageState extends ConsumerState<PreferenceListPage> {
                         itemTitle: _tests[index].result['type'] ?? '알 수 없음',
                         index: index,
                         onTap: () {
-
                           Navigator.push(
                             context,
                             CupertinoPageRoute(
