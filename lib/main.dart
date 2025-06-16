e54116c7e4f3093647743a9673d632f4aaed5a34
jinsung99123 <felixson99@gmail.com>
2025-06-16 10:27:09 +0900

    fix: 메인 다트 임포트 앱용으로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/date_symbol_data_http_request.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:travel_muse_app/firebase_options.dart';
 import 'package:travel_muse_app/views/splash/splash_page.dart';
 
