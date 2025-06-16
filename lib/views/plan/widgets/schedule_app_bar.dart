4f4527e5b1600f2c517b552f36185e054f2516d0
jinsung99123 <felixson99@gmail.com>
2025-06-16 14:11:41 +0900

    fix: 스케줄 페이지 뒤로가기 버튼 삭제

--- lib/views/plan/widgets/schedule_app_bar.dart
+++ lib/views/plan/widgets/schedule_app_bar.dart
@@ -10,17 +10,14 @@ class ScheduleAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: IconButton(
-        icon: const Icon(Icons.chevron_left),
-        onPressed: () => Navigator.of(context).pop(),
-      ),
-      title:  Text(
+      automaticallyImplyLeading: false,
+      title: Text(
         '여행 일정 등록',
         style: TextStyle(
           color: AppColors.grey[800],
           fontSize: 18,
           fontFamily: 'Pretendard',
-          fontWeight: FontWeight.w700
+          fontWeight: FontWeight.w700,
         ),
       ),
       centerTitle: true,
@@ -44,4 +41,3 @@ class ScheduleAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
-
