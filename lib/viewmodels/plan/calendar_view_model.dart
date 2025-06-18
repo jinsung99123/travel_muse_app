b42d5b31c2e5c93841c70cb419c8b505c08ddb5f
jinsung99123 <felixson99@gmail.com>
2025-06-19 01:38:54 +0900

    feat: 문서 주석 처리

--- lib/viewmodels/plan/calendar_view_model.dart
+++ lib/viewmodels/plan/calendar_view_model.dart
@@ -4,6 +4,8 @@ import 'package:travel_muse_app/models/plan/calendar_model.dart';
 class CalendarViewModel extends StateNotifier<CalendarState> {
   CalendarViewModel() : super(CalendarState(focusedDay: DateTime.now()));
 
+  /// startDay가 없거나, startDay와 endDay가 모두 있는 경우 → 새로운 선택 시작
+  /// startDay만 있는 경우 → endDay 설정
   void selectDay(DateTime selectedDay, DateTime newFocusedDay) {
     if (state.startDay == null ||
         (state.startDay != null && state.endDay != null)) {
@@ -35,10 +37,12 @@ class CalendarViewModel extends StateNotifier<CalendarState> {
     }
   }
 
+  /// 특정 날짜가 선택된 날짜인지 여부
   bool isSelected(DateTime day) {
     return day == state.startDay || day == state.endDay;
   }
 
+  /// 특정 날짜가 선택된 날짜 범위(startDay ~ endDay) 사이에 포함되는지 여부
   bool isBetween(DateTime day) {
     return state.startDay != null &&
         state.endDay != null &&
