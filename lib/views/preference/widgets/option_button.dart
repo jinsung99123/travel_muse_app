e22f520ddb8588b53f4bcc0ced0c0b4f7e9233a7
ghghtls <ghghtls2@gmail.com>
2025-06-18 22:52:32 +0900

    fix:unimport

--- lib/views/preference/widgets/option_button.dart
+++ lib/views/preference/widgets/option_button.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_muse_app/constants/app_colors.dart';
-import 'package:travel_muse_app/views/preference/preference_test_page.dart';
 
 class OptionButton extends StatelessWidget {
   const OptionButton({
