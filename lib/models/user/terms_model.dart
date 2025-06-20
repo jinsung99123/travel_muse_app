0409338a414c90c5e4d079643713fbf40a11008b
MJ <gwang907@gmail.com>
2025-06-20 13:28:48 +0900

    feat: 약관 모델 수정

--- lib/models/user/terms_model.dart
+++ lib/models/user/terms_model.dart
@@ -1,46 +1,46 @@
 class Terms {
   Terms({
-    required this.termId,
     required this.title,
     required this.content,
     required this.isRequired,
     required this.version,
     required this.createdAt,
+    required this.order,
   });
 
-  final String termId;
   final String title;
   final String content;
   final bool isRequired;
   final String version;
   final DateTime createdAt;
+  final int order;
 
   Terms copyWith({
-    String? termId,
     String? title,
     String? content,
     bool? isRequired,
     String? version,
     DateTime? createdAt,
+    int? order,
   }) {
     return Terms(
-      termId: termId ?? this.termId,
       title: title ?? this.title,
       content: content ?? this.content,
       isRequired: isRequired ?? this.isRequired,
       version: version ?? this.version,
       createdAt: createdAt ?? this.createdAt,
+      order: order ?? this.order,
     );
   }
 
   factory Terms.fromJson(String id, Map<String, dynamic> json) {
     return Terms(
-      termId: id,
       title: json['title'],
       content: json['content'],
       isRequired: json['isRequired'],
       version: json['version'],
       createdAt: DateTime.parse(json['createdAt']),
+      order: (json['order'] as num).toInt(),
     );
   }
 
@@ -50,5 +50,6 @@ class Terms {
     'isRequired': isRequired,
     'version': version,
     'createdAt': createdAt.toIso8601String(),
+    'order': order,
   };
 }
