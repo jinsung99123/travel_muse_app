0b7dddbc60bdd9d72f5e1bbe6010a8bbcfd3ca71
jinju9513 <wjswlswn99@naver.com>
2025-06-20 11:39:23 +0900

    fix :  디자인 수정및 아이콘 svg파일로 대체

--- lib/views/plan/plan/schedule/widgets/ai_button.dart
+++ lib/views/plan/plan/schedule/widgets/ai_button.dart
@@ -76,12 +76,8 @@ class AiButton extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              AppColors.primary[50]!,
-              AppColors.primary[300]!,
-              AppColors.primary[400]!
-            ],
-            stops: [0.0, 0.3, 0.7],
+            colors: [AppColors.primary[50]!, AppColors.primary[300]!, AppColors.primary[400]!],
+            stops: [0.0,0.3,0.7],
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
           ),
