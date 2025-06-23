56d7afb7c50574283f24ba442c3750f2deff0101
jinsung99123 <felixson99@gmail.com>
2025-06-23 19:55:00 +0900

    feat: 알림 helper 임시 설정

---
+++ lib/utills/notification_helper.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+@pragma('vm:entry-point')
+void notificationTapOnBackGround(NotificationResponse response) {
+  print(response.payload);
+}
+
+class NotificationHelper {
+  static final flutterNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async {
+    final darwinSetting = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestSoundPermission: true,
+      requestBadgePermission: true,
+    );
+    final initSetting = InitializationSettings(iOS: darwinSetting);
+
+    await flutterNotificationsPlugin.initialize(
+      initSetting,
+      onDidReceiveNotificationResponse: (details) {},
+      onDidReceiveBackgroundNotificationResponse: notificationTapOnBackGround,
+    );
+  }
+
+  static Future<void> show(String title, String content) async {
+    await flutterNotificationsPlugin.show(
+      0,
+      title,
+      content,
+      NotificationDetails(
+        iOS: DarwinNotificationDetails(
+          presentSound: true,
+          presentAlert: true,
+          presentBadge: true,
+        ),
+      ),
+      payload: 'hi',
+    );
+  }
+}
