8339bfc62f85662b8f81575cb7ef623a44af07fa
jinsung99123 <felixson99@gmail.com>
2025-06-23 19:58:10 +0900

    feat: 메인 다트 알림 초기화 설정

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,14 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:travel_muse_app/firebase_options.dart';
+import 'package:travel_muse_app/utills/notification_helper.dart';
 import 'package:travel_muse_app/views/user/splash/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: '.env');
+  await NotificationHelper.init();
 
   // Firebase Crashlytics 에러 핸들링 추가
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
