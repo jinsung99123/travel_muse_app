f62e77770a0d16c3f275b5e3c419d3c6cb7779b6
MJ <gwang907@gmail.com>
2025-06-23 20:55:37 +0900

    feat: custom dialog 위젯(WIP)

--- lib/views/my_page/widgets/delete_account_button.dart
+++ lib/views/my_page/widgets/delete_account_button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:travel_muse_app/constants/app_colors.dart';
 import 'package:travel_muse_app/providers/user/auth_view_model_provider.dart';
 import 'package:travel_muse_app/views/user/login/login_page.dart';
+import 'package:travel_muse_app/views/my_page/widgets/custom_dialog.dart';
 
 class DeleteAccountButton extends ConsumerWidget {
   const DeleteAccountButton({super.key});
@@ -10,14 +12,39 @@ class DeleteAccountButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton(
       onPressed: () {
-        // TODO: 탈퇴 처리 전 확인 pop up
         ref.read(authViewModelProvider.notifier).deleteAccount();
         Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(builder: (_) => LoginPage()),
           (route) => false,
         );
+        // 탈퇴 전 확인 팝업(WIP)
+        // showGeneralDialog(
+        //   context: context,
+        //   barrierDismissible: true,
+        //   barrierLabel: 'Dismiss',
+        //   barrierColor: AppColors.black.withAlpha(200),
+        //   transitionDuration: const Duration(milliseconds: 200),
+        //   pageBuilder: (context, animation, secondaryAnimation) {
+        //     return GestureDetector(
+        //       onTap: () => Navigator.of(context).pop(),
+        //       child: Material(
+        //         type: MaterialType.transparency,
+        //         child: Center(
+        //           child: GestureDetector(
+        //             onTap: () {},
+        //             child: Padding(
+        //               padding: const EdgeInsets.symmetric(horizontal: 16),
+        //               child: CustomDialog(),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     );
+        //   },
+        // );
       },
+
       child: Text('회원 탈퇴'),
     );
   }
