1a31d72078d51081ef0d0cf36a170cb191be44da
jinju9513 <wjswlswn99@naver.com>
2025-06-24 15:19:10 +0900

    feat: 신고된 게시글 상세보기 연결

--- lib/views/post/report/admin_report_screen.dart
+++ lib/views/post/report/admin_report_screen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:travel_muse_app/models/scoial/reported_post.dart';
 import 'package:travel_muse_app/models/scoial/reproted_comment.dart';
+import 'package:travel_muse_app/providers/post/post_provider.dart';
 import 'package:travel_muse_app/providers/scoial/admin_report_provider.dart';
 import 'package:travel_muse_app/viewmodels/scoial/widgets/reported_card.dart';
-import 'package:travel_muse_app/views/post/post_page.dart';
+import 'package:travel_muse_app/views/post/post_detail_page.dart';
 
 class AdminReportScreen extends ConsumerWidget {
   const AdminReportScreen({super.key});
@@ -26,16 +27,27 @@ class AdminReportScreen extends ConsumerWidget {
                 if (item is ReportedPost) {
                   return ReportedCard(
                     title: '[게시글] ${item.content}',
-                    subtitle: '작성자: ${item.userId}\n신고 수: ${item.reportCount}',
-                    onTap: () {
-                      // 해당게시글 페이지로 이동
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (_) => PostPage(postId: item.postId),
-                      //   ),
-                      // );
+                    subtitle: '작성자: ${item.nickname}\n신고 수: ${item.reportCount}',
+                    onTap: () async {
+                      final postRepo = ref.read(postRepositoryProvider);
+                      final post = await postRepo.fetchPostById(
+                        item.postId,
+                      ); 
+
+                      if (post != null && context.mounted) {
+                       await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => PostDetailPage(post: post),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('게시글을 불러올 수 없습니다.')),
+                        );
+                      }
                     },
+
                     onDelete: () {
                       print('[UI] onDelete 호출됨 - postId: ${item.postId}');
                       viewModel.deletePost(item.postId);
@@ -46,7 +58,7 @@ class AdminReportScreen extends ConsumerWidget {
                   return ReportedCard(
                     title: '[댓글] ${item.content}',
                     subtitle:
-                        '작성자: ${item.userId}\n게시글 ID: ${item.postId}\n신고 수: ${item.reportCount}',
+                        '작성자: ${item.nickname}\n게시글 ID: ${item.postId}\n신고 수: ${item.reportCount}',
                     onTap: () {
                       //해당 댓글로 이동
                       // Navigator.push(
