d57ac4e1c3f14464c735577d8db8313aca106f0e
jinju9513 <wjswlswn99@naver.com>
2025-06-24 10:15:13 +0900

    feat: 사용자 신고 처리 로직 구현 및 Firestore 연동

--- lib/viewmodels/scoial/report_view_model.dart
+++ lib/viewmodels/scoial/report_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:travel_muse_app/models/scoial/report.dart';
 import 'package:travel_muse_app/repositories/scoial/report_repository.dart';
@@ -6,7 +7,6 @@ class ReportViewModel extends StateNotifier<AsyncValue<void>> {
   ReportViewModel(this.repository) : super(const AsyncValue.data(null));
 
   final ReportRepository repository;
-
 
   /// 사용자의 신고 데이터를 처리
   /// 중복 신고 시 예외가 발생하며, 상태는 AsyncValue로 반영됩니다.
@@ -17,9 +17,9 @@ class ReportViewModel extends StateNotifier<AsyncValue<void>> {
     required String targetOwnerId,
     required String reasonCode,
     String? reasonText,
+    String? postId, // 댓글일 경우에만 필요
   }) async {
     state = const AsyncValue.loading();
-
     try {
       final report = Report(
         targetType: targetType,
@@ -32,6 +32,21 @@ class ReportViewModel extends StateNotifier<AsyncValue<void>> {
       );
 
       await repository.submitReport(report);
+      ///신고 대상 문서 업데이트
+      final docRef =
+          targetType == 'post'
+              ? FirebaseFirestore.instance.collection('posts').doc(targetId)
+              : FirebaseFirestore.instance
+                  .collection('posts')
+                  .doc(postId!) // 댓글일 경우 postId가 필요
+                  .collection('comments')
+                  .doc(targetId);
+
+      await docRef.update({
+        'reportCount': FieldValue.increment(1),
+        'isReported': true,
+      });
+
       state = const AsyncValue.data(null);
     } catch (e, st) {
       state = AsyncValue.error(e, st);
