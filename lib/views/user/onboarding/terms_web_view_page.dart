e01e642f5976277ccd9ea9edc60bfebcfa22ceb1
jinsung99123 <felixson99@gmail.com>
2025-06-24 16:43:24 +0900

    fix: 웹뷰 db에 설정 및 웹뷰 중복 방지 키 설정

--- lib/views/user/onboarding/terms_web_view_page.dart
+++ lib/views/user/onboarding/terms_web_view_page.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class TermsWebViewPage extends StatelessWidget {
+class TermsWebViewPage extends StatefulWidget {
   const TermsWebViewPage({super.key, required this.title, required this.url});
+
   final String title;
   final String url;
 
   @override
-  Widget build(BuildContext context) {
-    final controller =
+  State<TermsWebViewPage> createState() => _TermsWebViewPageState();
+}
+
+class _TermsWebViewPageState extends State<TermsWebViewPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
         WebViewController()
           ..setJavaScriptMode(JavaScriptMode.unrestricted)
-          ..loadRequest(Uri.parse(url));
+          ..loadRequest(Uri.parse(widget.url));
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title, style: TextStyle(color: Colors.black)),
+        title: Text(widget.title, style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         elevation: 1,
       ),
-      body: WebViewWidget(controller: controller),
+      body: WebViewWidget(key: UniqueKey(), controller: _controller),
     );
   }
 }
