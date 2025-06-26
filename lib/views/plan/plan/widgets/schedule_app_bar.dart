26c42988658eea096df7291c24bebcba8ba5b881
jinju9513 <wjswlswn99@naver.com>
2025-06-26 18:24:36 +0900

    refactor: 뒤로가기 버튼 다이얼로그 연동

--- lib/views/plan/plan/widgets/schedule_app_bar.dart
+++ lib/views/plan/plan/widgets/schedule_app_bar.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:travel_muse_app/constants/app_colors.dart';
 import 'package:travel_muse_app/providers/plan/schedule/schedule_provider.dart';
 import 'package:travel_muse_app/views/plan/location/map_page.dart';
+import 'package:travel_muse_app/views/plan/plan/schedule/widgets/schedule_confirm_dialog.dart';
 
 class ScheduleAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const ScheduleAppBar({super.key, required this.planId});
@@ -15,8 +16,22 @@ class ScheduleAppBar extends ConsumerWidget implements PreferredSizeWidget {
     return AppBar(
       leading: IconButton(
         icon: const Icon(Icons.chevron_left),
-        onPressed: () => Navigator.of(context).pop(),
+        onPressed: () async {
+          final shouldPop = await showDialog<bool>(
+            context: context,
+            builder:
+                (context) => const ScheduleConfirmDialog(
+                  title: '일정 등록 화면에서 나가시겠습니까?',
+                  description: '지금 나가면 일정이 저장되지 않아요',
+                ),
+          );
+
+          if (shouldPop == true) {
+            Navigator.of(context).pop();
+          }
+        },
       ),
+
       title: Text(
         '여행 일정 등록',
         style: TextStyle(
@@ -31,14 +46,16 @@ class ScheduleAppBar extends ConsumerWidget implements PreferredSizeWidget {
         Padding(
           padding: const EdgeInsets.only(right: 16),
           child: GestureDetector(
-              child: SvgPicture.asset('assets/icons/map.svg', 
-                    width: 28,
-                    height: 28,),
-             onTap: () async {
+            child: SvgPicture.asset(
+              'assets/icons/map.svg',
+              width: 28,
+              height: 28,
+            ),
+            onTap: () async {
               final hasRoute = await ref
                   .read(scheduleViewModelProvider.notifier)
                   .hasRoute(planId);
-          
+
               if (!hasRoute) {
                 if (context.mounted) {
                   ScaffoldMessenger.of(context).showSnackBar(
@@ -50,9 +67,7 @@ class ScheduleAppBar extends ConsumerWidget implements PreferredSizeWidget {
               if (context.mounted) {
                 await Navigator.push(
                   context,
-                  MaterialPageRoute(
-                    builder: (_) => MapPage(planId: planId),
-                  ),
+                  MaterialPageRoute(builder: (_) => MapPage(planId: planId)),
                 );
               }
             },
