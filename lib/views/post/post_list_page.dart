b2d8efa7ce6d72b1c4d14c902f4068068e19c35c
jinju9513 <wjswlswn99@naver.com>
2025-06-27 11:46:15 +0900

    fix : appbar 아이콘 svg로 교체

--- lib/views/post/post_list_page.dart
+++ lib/views/post/post_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:travel_muse_app/constants/app_colors.dart';
 import 'package:travel_muse_app/constants/app_text_styles.dart';
 import 'package:travel_muse_app/core/widgets/bottom_bar.dart';
 import 'package:travel_muse_app/views/post/widgets/list/post_list_view.dart';
@@ -23,11 +25,31 @@ class _PostListPageState extends State<PostListPage> {
         title: const Text('커뮤니티'),
         centerTitle: false,
         titleTextStyle: AppTextStyles.appBarTitle,
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Center(
+            child: IconButton(
+              icon: SvgPicture.asset(
+                'assets/icons/chevron-left.svg',
+                width: 24,
+                height: 24,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ),
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 15),
             child: IconButton(
-              icon: const Icon(Icons.search),
+              icon: SvgPicture.asset(
+                'assets/icons/search.svg',
+                width: 24,
+                height: 24,
+                color: AppColors.grey[700], 
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
