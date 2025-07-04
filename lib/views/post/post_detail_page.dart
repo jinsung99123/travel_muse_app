f851ce95eb0fed8d99eb39ca746478276470424f
ghghtls <ghghtls2@gmail.com>
2025-07-04 13:54:52 +0900

    fix:comment 공백제거 임포트 변경

--- lib/views/post/post_detail_page.dart
+++ lib/views/post/post_detail_page.dart
@@ -11,7 +11,7 @@ import 'package:travel_muse_app/providers/post/like_provider.dart';
 import 'package:travel_muse_app/providers/post/post_list_view_model_provider.dart';
 import 'package:travel_muse_app/providers/post/post_provider.dart';
 import 'package:travel_muse_app/providers/scoial/report_provider.dart';
-import 'package:travel_muse_app/views/post/%08comment/comment_section.dart';
+import 'package:travel_muse_app/views/post/comment/comment_section.dart';
 import 'package:travel_muse_app/views/post/post_write_page.dart';
 import 'package:travel_muse_app/views/post/widgets/detail/place_preview_card.dart';
 import 'package:travel_muse_app/views/post/widgets/detail/post_detail_content.dart';
@@ -183,9 +183,7 @@ class _PostDetailPageState extends ConsumerState<PostDetailPage> {
                                 .deletePost(currentPost.postId);
 
                             await ref
-                                .read(
-                                  postListViewModelProvider.notifier,
-                                )
+                                .read(postListViewModelProvider.notifier)
                                 .fetchInitialPosts();
 
                             if (mounted) {
