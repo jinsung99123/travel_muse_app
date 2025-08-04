a84497166b2cdf49685d1b982bf32888fa49503d
jinsung99123 <felixson99@gmail.com>
2025-08-05 01:19:16 +0900

    fix: 댓글, 지역/캘린더 레포지토리 리펙토링

--- lib/repositories/plan/calendar_location_repository.dart
+++ lib/repositories/plan/calendar_location_repository.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:travel_muse_app/models/plan/planstate.dart';
 
 class CalendarLocationRepository {
@@ -12,11 +11,11 @@ class CalendarLocationRepository {
   Future<void> savePlan({
     required String planId,
     required DateTime startDate,
-    required DateTime? endDate, // 수정: nullable 허용
+    required DateTime? endDate,
     required String region,
     required String userId,
   }) async {
-    final actualEndDate = endDate ?? startDate; // 하루짜리 처리
+    final actualEndDate = endDate ?? startDate;
     final duration = actualEndDate.difference(startDate).inDays + 1;
 
     await _firestore.collection('plans').doc(planId).set({
@@ -31,11 +30,11 @@ class CalendarLocationRepository {
   /// 새 planId를 생성하고 플랜 데이터를 Firestore에 저장
   Future<String> createAndSavePlan({
     required DateTime startDate,
-    required DateTime? endDate, // 수정: nullable 허용
+    required DateTime? endDate,
     required String region,
     required String userId,
   }) async {
-    final actualEndDate = endDate ?? startDate; // 하루짜리 처리
+    final actualEndDate = endDate ?? startDate;
     final duration = actualEndDate.difference(startDate).inDays + 1;
 
     final docRef = _firestore.collection('plans').doc();
